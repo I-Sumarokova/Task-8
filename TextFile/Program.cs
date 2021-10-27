21c68065d9caccf13e6f24804199da6138c02ad9
Ирина Сумарокова <irina.sumarokova92@yandex.ru>
2021-10-27 17:15:46 +0300

    Добавьте файлы проекта.

---
+++ TextFile/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "File";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string path2 = "File/Sum.txt";
+            if (!File.Exists(path2))
+            {
+                File.Create(path2);
+            }
+
+            string path3 = "File/Sum.txt";
+            using (StreamWriter sw = new StreamWriter(path3, false))
+            {
+                sw.WriteLine("Массив случайных чисел:");
+                int[] array = new int[100];
+                Random random = new Random();
+                int S = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    array[i] = random.Next(0, 10);
+                    S += array[i];
+                    sw.Write("{0} ", array[i]);
+                }
+                sw.WriteLine("\nСумма чисел равна = {0}", S);
+            }
+
+            using (StreamReader sr = new StreamReader(path3))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Занятие 8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Занятие_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\User\\Desktop\\Lab8\\";
+            if (Directory.Exists(path))
+            {
+                string[] paths = Directory.GetDirectories(path);
+                foreach (string s in paths)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Каталоги:");
+                    Console.WriteLine(s);
+                    Console.WriteLine();
+
+                    Console.WriteLine("Подкаталоги:");
+                    string[] path2 = Directory.GetDirectories(s);
+                    foreach (string s1 in path2)
+                    {
+                        Console.Write(s1);
+                        Console.WriteLine();
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
